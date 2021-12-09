d9b99c9888d41f589a9397e4dd0cf21217b4387b
shonenjunk <zachtyler8@gmail.com>
2021-12-09 15:00:42 -0600

    SyncTheory working build with improved comments

--- app/src/main/java/com/example/synctheory/CourseHelperClass.java
+++ app/src/main/java/com/example/synctheory/CourseHelperClass.java
@@ -1,16 +1,21 @@
 package com.example.synctheory;
 
+//helper class that helps encode newly added courses to the proper JSON format required by Firebase
 public class CourseHelperClass {
 
+    //course name
     String courseName;
 
+    //required empty constructor
     public CourseHelperClass(){
     }
 
+    //constructor called to create the properly encoded course object based on the course name given by the teacher.
     public CourseHelperClass(String courseName) {
         this.courseName = courseName;
     }
 
+    //getter and setter for the course name (unused)
     public String getCourseName() {
         return courseName;
     }
