eda50d595b431a21e84688728f55a85540591035
shonenjunk <zachtyler8@gmail.com>
2021-12-09 15:12:24 -0600

    SyncTheory working build with improved comments

--- app/src/main/java/com/example/synctheory/SignUp/SignUpViewFragment.java
+++ app/src/main/java/com/example/synctheory/SignUp/SignUpViewFragment.java
@@ -17,6 +17,7 @@ import com.example.synctheory.R;
 public class SignUpViewFragment extends Fragment implements SignUpContract.View{
 
     private SignUpContract.Presenter mPresenter;
+    //UI elements in the sign up xml view
     EditText etEmail;
     EditText etNumber;
     EditText etPassword;
@@ -45,15 +46,22 @@ public class SignUpViewFragment extends Fragment implements SignUpContract.View{
         // Inflate the layout for this fragment
         View root =  inflater.inflate(R.layout.fragment_sign_up_view, container, false);
 
+        //assign the email edittext to appopriate UI element
         etEmail = root.findViewById(R.id.etEmail);
 
+        //assign the phoneNumber edittext to appopriate UI element
         etNumber = root.findViewById(R.id.etPhoneNumber);
 
+        //assign the password edittext to appopriate UI element
         etPassword = root.findViewById(R.id.etPassword);
 
+        //assign the elevated user checkbox var to appopriate UI element
         checkElevUser = root.findViewById(R.id.checkElevUser);
 
+        //assigns signup button var to the button defined in XML
         btnSignUp = root.findViewById(R.id.btnSignUp);
+
+        //assign the button click listener to notify presenter when the sign up button is pressed
         btnSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -61,7 +69,10 @@ public class SignUpViewFragment extends Fragment implements SignUpContract.View{
             }
         });
 
+        //assigns Back button var to the button defined in XML
         btnBack = root.findViewById(R.id.btnBack);
+
+        //assign the button click listener to notify presenter when the back button is pressed
         btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -75,12 +86,14 @@ public class SignUpViewFragment extends Fragment implements SignUpContract.View{
     @Override
     public void onResume(){ super.onResume(); }
 
+    //method that closes the sign up activity when the user presses sign up button
     @Override
     public void finishSignUpActivity() {
         getActivity().finish();
     }
 
 
+    //getters for the edit text values with error checking if user doesn't enter anything
     @Override
     public String getEtEmail() {
         if(this.etEmail.getText() == null){
@@ -109,11 +122,14 @@ public class SignUpViewFragment extends Fragment implements SignUpContract.View{
         }
     }
 
+    //returns the value of the check elevated user boolean
     @Override
     public Boolean getCheckElevUser() { return checkElevUser.isChecked(); }
 
+    //setter for the sign up presenter
     @Override
     public void setPresenter(SignUpContract.Presenter presenter) { mPresenter = presenter; }
+    //setters for the sign up view edit texts
     @Override
     public void setEtEmail(String email) { this.etEmail.setText(email); }
     @Override
