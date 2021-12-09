07cf996684db399e75981ffdefcf72aad833b7d1
shonenjunk <zachtyler8@gmail.com>
2021-12-09 14:58:52 -0600

    SyncTheory working build with improved comments

--- app/src/main/java/com/example/synctheory/LectureHelperClass.java
+++ app/src/main/java/com/example/synctheory/LectureHelperClass.java
@@ -1,18 +1,23 @@
 package com.example.synctheory;
 
+//helper class that helps encode newly added lectures to the proper JSON format required by Firebase
 public class LectureHelperClass {
 
+    //lecture data
     String lectureName;
     String note;
 
+    //required empty constructor
     public LectureHelperClass(){
     }
 
+    //constructor called to create the lecture object based on the lecture name given by the teacher
     public LectureHelperClass(String lectureName, String note) {
         this.lectureName = lectureName;
         this.note = note;
     }
 
+    //general getters and setters for each of the various lecture attributes (unused)
     public String getLectureName() {
         return lectureName;
     }
